9bed1e148a52300e3d7fbe7004a4f651502a0b7e
Sanyogita45 <sanyogita.24bce10550@vitbhopal.ac.in>
2026-03-18 13:48:44 +0530

    update(1)

---
+++ Day6-SupportVectorMachine(SVM)/svm.py
@@ -0,0 +1,103 @@
+# Importing Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Import dataset
+dataset = pd.read_csv('customer_subscription.csv')
+
+# Features (Age, Income)
+X =dataset.iloc[:, [0,1]].values
+# Target (Subscription)
+Y = dataset.iloc[:, 3].values
+
+# Splitting datasets
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train) # fit_transform -- standardisation bhi aur training bhi
+X_test = sc.transform(X_test) 
+
+# Training SVM (linear kernel)
+from sklearn.svm import SVC
+#classifier = SVC(kernel='linear', random_state=0) # 2 classes ko ek straight line se separate karta hai
+classifier = SVC(kernel='rbf',C=100, gamma=0.1)    # curved boundary  #for better results and accuracy
+classifier.fit(X_train, Y_train)
+
+# Prediction
+Y_pred = classifier.predict(X_test)
+
+# Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(Y_test, Y_pred)
+print(cm)
+print("Accuracy: ", accuracy_score(Y_test, Y_pred))
+
+# Visualisation (Training Set)
+from matplotlib.colors import ListedColormap
+
+X_set, y_set = X_train, Y_train
+
+X1, X2 = np.meshgrid(
+    np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+    np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01)
+)
+
+plt.contourf(
+    X1, X2,
+    classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+    alpha=0.75,
+    cmap=ListedColormap(('red', 'green'))
+)
+
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(
+        X_set[y_set == j, 0],
+        X_set[y_set == j, 1],
+        color=ListedColormap(('red', 'green'))(i),
+        label=j
+    )
+
+plt.title('SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Income')
+plt.legend()
+plt.show()
+
+# Visualisation (Test Set)
+X_set, y_set = X_test, Y_test
+
+X1, X2 = np.meshgrid(
+    np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+    np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01)
+)
+
+plt.contourf(
+    X1, X2,
+    classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+    alpha=0.75,
+    cmap=ListedColormap(('red', 'green'))
+)
+
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(
+        X_set[y_set == j, 0],
+        X_set[y_set == j, 1],
+        color=ListedColormap(('red', 'green'))(i),
+        label=j
+    )
+
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Income')
+plt.legend()
+plt.show()
